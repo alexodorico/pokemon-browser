2ad04b301d78024adea22fdcaf1a48918f30416c
Alex Odorico <alex.odorico@morleynet.com>
2019-07-25 10:44:44 -0400

    Fetches all pokemon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./main.scss";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
+  function getPokemon(offset = 0) {
+    if (offset !== 1000) {
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=50`)
+        .then(response =>
+          setPokemon(pokemon => [...pokemon, ...response.data.results])
+        )
+        .then(_ => getPokemon(offset + 50));
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header" />
